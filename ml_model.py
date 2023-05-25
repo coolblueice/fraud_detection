a942ddb65174b27b1c85ee40e101e88746e86e1d
coolblueice <vineetjuneja@msn.com>
2023-05-25 22:59:55 +0530

    model 4

--- ml_model.py
+++ ml_model.py
@@ -20,5 +20,5 @@ def model4():
     print ("adding code for ML model 4")
 
 
-print ("model 3 is performing best!")
+print ("model 2 is performing best!")
 print ("This was after running testing data")
